bea0d984edd8826efcc8cbb22bd49630e295812f
Deepak <deeepakjallepalli26@gmail.com>
2024-02-09 14:31:46 +0530

    INT 219

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.write("this is javascript");
+        document.getElementById("js").innerHTML = "this is javascript";
+
+        function msg1() {
+            alert("hello world");
+        }
